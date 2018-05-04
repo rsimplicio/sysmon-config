5200e842881778448135ac0bd58b9551ef8d655c
Robert Simplicio <Robert.Simplicio@kernmedical.com>
2018-05-04 14:23:29 -0700

    Change urls to correct repo

---
+++ Auto_Update.bat
@@ -0,0 +1,5 @@
+@echo on
+cd C:\ProgramData\sysmon\
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rsimplicio/sysmon-config/master/sysmonconfig-export.xml')"
+sysmon64 -c sysmonconfig-export.xml
+exit
